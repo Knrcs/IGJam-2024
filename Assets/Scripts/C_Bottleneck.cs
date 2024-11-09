9af9aebb9b70ad2d5e696aa598098a2cdc7c3e9a
Knrc <kontakt.mikey@gmail.com>
2024-11-09 13:05:07 +0100

    Spine package Import

--- Assets/Scripts/C_Bottleneck.cs
+++ Assets/Scripts/C_Bottleneck.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using  UnityEngine.UI;
 
 public class C_Bottleneck : MonoBehaviour
 {
+    public TMP_Text multiplyerText;
+    public GameObject[] cardStack;
     public Image bottleNeckFill;
-    
+
     // Start is called before the first frame update
     void Start()
     {
